19b0763d19497a4b2da675a62c4ba82cdecf5aae
Corwin Wright <git.hub@corwin.me.uk>
2020-07-20 19:10:04 +0100

    version 4 of 2d+1 st

--- 02Generalise/plot_examples.m
+++ 02Generalise/plot_examples.m
@@ -16,13 +16,22 @@ clearvars
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%data to analyse
-% Settings.Day = datenum(2008,1,127);
-% Settings.GiD = [56];
+% % %data to analyse
+Settings.Day = datenum(2008,1,127);
+Settings.GiD = [56];
+Settings.SampleIndices = [174,34];
 
 
-Settings.Day = datenum(2010,10,16);
-Settings.GiD = 186;
+% % Settings.Day = datenum(2010,10,16);
+% % Settings.GiD = 186;
+% % Settings.SampleIndices = [128,38];
+
+% Settings.Day = datenum(2007,1,13);
+% Settings.GiD = [122];
+% Settings.SampleIndices = [78,57];
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% load data as a granule-pair
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,22 +41,22 @@ Airs  = cat_struct(prep_airs_3d(Settings.Day,Settings.GiD,  'PreSmooth',[3,3,1])
                    prep_airs_3d(Settings.Day,Settings.GiD+1,'PreSmooth',[3,3,1]), ...
                    2,{'MetaData','Source','ret_z'});
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% 3dst the granule-pair
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 [ST3D,Airs] = gwanalyse_airs_3d(Airs,'TwoDPlusOne',true);
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% plot some maps
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-z = 50;
+z = 30;
 zidx = closest(z,Airs.ret_z);
 z = Airs.ret_z(zidx);
 
-
+figure(1)
 clf
 set(gcf,'color','w')
 redyellowblue32
@@ -57,9 +66,9 @@ pcolor(Airs.l1_lon,Airs.l1_lat,squeeze(ST3D.A(:,:,zidx))); shading flat; colorba
 title(['Amplitude, 3DST, ',num2str(z),'km altitude'])
 
 
-subplot(2,2,2)
-pcolor(Airs.l1_lon,Airs.l1_lat,squeeze(ST3D.A_2dp1(:,:,zidx))); shading flat; colorbar
-title(['Amplitude, 2D+1, ',num2str(z),'km altitude'])
+% % subplot(2,2,2)
+% % pcolor(Airs.l1_lon,Airs.l1_lat,squeeze(ST3D.A_2dp1(:,:,zidx))); shading flat; colorbar
+% % title(['Amplitude, 2D+1, ',num2str(z),'km altitude'])
 
 subplot(2,2,3)
 pcolor(Airs.l1_lon,Airs.l1_lat,1./squeeze(ST3D.m(:,:,zidx))); shading flat; colorbar
@@ -79,28 +88,34 @@ sgtitle([datestr(Settings.Day),', g',num2str(Settings.GiD)])
 %% plot some profiles
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-subplot(1,3,1)
+figure(2); set(gcf,'color','w'); clf
+
+subplot(1,4,1)
+plot(squeeze(ST3D.IN(Settings.SampleIndices(2),Settings.SampleIndices(1),:)),Airs.ret_z,'color','r')
+
+
+subplot(1,4,2)
 A.ST3D = ST3D.A; A.ST3D(A.ST3D < 1.5) = NaN;
 plot(squeeze(nanmean(A.ST3D,[1,2])),Airs.ret_z,'color','r')
 hold on
-A.ST2D = ST3D.A_2dp1; A.ST2D(A.ST2D < 1.5) = NaN;
-plot(squeeze(nanmean(A.ST2D,[1,2])),Airs.ret_z,'color','k')
-title('Amplitude')
+% % A.ST2D = ST3D.A_2dp1; A.ST2D(A.ST2D < 1.5) = NaN;
+% % plot(squeeze(nanmean(A.ST2D,[1,2])),Airs.ret_z,'color','k')
+% % title('Amplitude')
 
-subplot(1,3,2)
+subplot(1,4,3)
 A.ST3D = ST3D.A; m = ST3D.m; m(A.ST3D < 1.5) = NaN;
 plot(1./squeeze(nanmean(m,[1,2])),Airs.ret_z,'color','r')
 hold on
-A.ST2D = ST3D.A_2dp1; m = ST3D.m_2dp1; m(A.ST2D < 1.5) = NaN;
+A.ST2D = ST3D.A; m = abs(ST3D.m_2dp1); m(A.ST2D < 1.5) = NaN;
 plot(squeeze(1./nanmean(m,[1,2])),Airs.ret_z,'color','k')
 title('Lz')
 xlim([10 40])
 
-subplot(1,3,3)
-A.ST3D = ST3D.A; k = ST3D.k; k(A.ST3D < 1.5) = NaN;
-plot(1./squeeze(nanmean(k,[1,2])),Airs.ret_z,'color','r')
-hold on
-A.ST2D = ST3D.A_2dp1; k = ST3D.k_2dp1; k(A.ST2D < 1.5) = NaN;
-plot(squeeze(1./nanmean(k,[1,2])),Airs.ret_z,'color','k')
-title('k^{-1}')
-xlim([300 1000])
+% % subplot(1,4,4)
+% % A.ST3D = ST3D.A; k = ST3D.k; k(A.ST3D < 1.5) = NaN;
+% % plot(1./squeeze(nanmean(k,[1,2])),Airs.ret_z,'color','r')
+% % hold on
+% % A.ST2D = ST3D.A_2dp1; k = ST3D.k_2dp1; k(A.ST2D < 1.5) = NaN;
+% % plot(squeeze(1./nanmean(k,[1,2])),Airs.ret_z,'color','k')
+% % title('k^{-1}')
+% % xlim([0 2000])
