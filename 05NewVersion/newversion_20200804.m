4f3fabbfd2ab9bd7eeb9e90a94081f4183628d58
Corwin Wright <git.hub@corwin.me.uk>
2020-08-05 16:40:29 +0100

    continued work on new version

--- 05NewVersion/newversion_20200804.m
+++ 05NewVersion/newversion_20200804.m
@@ -19,35 +19,35 @@ clearvars
 %granule
 %%%%%%%%%%%%%%%%%%%%%%
 
-% % Settings.Day = datenum(2008,1,127);
-% % Settings.GiD = [56];
+Settings.Day = datenum(2008,1,127);
+Settings.GiD = [56];
 
 % % Settings.Day = datenum(2010,10,16);
 % % Settings.GiD = 186;
 
-Settings.Day = datenum(2007,1,13);
-Settings.GiD = [122];
+% % Settings.Day = datenum(2007,1,13);
+% % Settings.GiD = [122];
 
 
 %analysis
 %%%%%%%%%%%%%%%%%%%%%%
 
-Settings.BasisLevel = 40;            %km - nearest level taken
-Settings.c1         = [1,1,1].*0.5;  %c for first pass
-Settings.c2         = [1,1,1].*0.25; %c for second pass
-Settings.NPeaks     = 3;             %number of spectral peaks to identify for each spatial point
-Settings.Threshold  = 0.05;          %threshold in spectral space to be identified as a peak. this is a test and i am unsure of units yet, needs more work!
-
+Settings.BasisLevel = 40;                       %km - nearest level taken
+Settings.c1         = [1,1,1].*0.5;             %c for first pass
+Settings.c2         = [1,1,1].*0.25;            %c for second pass
+Settings.NPeaks     = 3;                        %number of spectral peaks to identify for each spatial point
+Settings.Threshold  = 1;                        %threshold in spectral space to be identified as a peak. this is an empirical value from testing and I am unsure of units yet, needs more work!
+Settings.Filt       = fspecial('gaussian',9,3); %characteristic size of point in spectral space. This is a little fatter than the default, to avoid very close peaks.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% get data
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Airs  = prep_airs_3d(Settings.Day,Settings.GiD,  'PreSmooth',[3,3,1]);
+% Airs  = prep_airs_3d(Settings.Day,Settings.GiD,  'PreSmooth',[3,3,1]);
 
-% % Airs  = cat_struct(prep_airs_3d(Settings.Day,Settings.GiD,  'PreSmooth',[3,3,1]),   ...
-% %                    prep_airs_3d(Settings.Day,Settings.GiD+1,'PreSmooth',[3,3,1]), ...
-% %                    2,{'MetaData','Source','ret_z'});
+Airs  = cat_struct(prep_airs_3d(Settings.Day,Settings.GiD,  'PreSmooth',[3,3,1]),   ...
+                   prep_airs_3d(Settings.Day,Settings.GiD+1,'PreSmooth',[3,3,1]), ...
+                   2,{'MetaData','Source','ret_z'});
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -70,29 +70,27 @@ Basis.ST = gwanalyse_airs_2d(Airs,Airs.ret_z(zidx), ...
 %find spectral peaks
 PeakStore = NaN([size(Airs.l1_lat),Settings.NPeaks,3]);
 
-%temporary warning as logic below doesn't work yet...
-disp('NEED TO ADAPT PEAK FINDER TO SPECTRAL SPACE MAPPING')
-
 for iX=1:1:size(Airs.l1_lat,1);
   for iY=1:1:size(Airs.l1_lat,2);
     
     %pull out the frame
     Frame = abs(Basis.ST.ST(:,:,iX,iY));
     
-% %     %sometimes the dynamic range is very low, usually if there is a 
-% %     %really big wave present. Scaling should increase the d-range.
-% %     Frame = 10.^(Frame);
-    
     %find maxima   
-    cent = FastPeakFind(Frame);
+    cent = FastPeakFind(Frame,0,Settings.Filt);
     if numel(cent) == 0; continue; end    
     %convert from list to [x1,y1;x2,y2;...];
     Cent(:,1) = cent(1:2:end); %position on k_(along-track)  axis
     Cent(:,2) = cent(2:2:end); %position on k_(across-track) axis
     clear cent
-
+    
     %find values of these indices
     for iZ=1:1:size(Cent,1); Cent(iZ,3) = Frame(Cent(iZ,2),Cent(iZ,1)); end
+    
+    %remove small values
+    Good = find(Cent(:,3) > Settings.Threshold);
+    Cent = Cent(Good,:);
+    clear Good
        
     %sort the data, select the number of peaks desired, and store
     [~,idx] = sort(Cent(:,3),'desc');
@@ -103,6 +101,18 @@ for iX=1:1:size(Airs.l1_lat,1);
     PeakStore(iX,iY,1:size(Cent,1),:) = Cent;
     clear Cent
     
+    
+% % % %     %convert to wavelengths (for sanity testing, not used in "production")
+% % % %     f1 = Basis.ST.freqs{1};   f2 = Basis.ST.freqs{2}; 
+% % % %     for iZ=1:1:Settings.NPeaks
+% % % %       if ~isnan(PeakStore(iX,iY,iZ,3));
+% % % %         PeakStore(iX,iY,iZ,1) = 1./f2(PeakStore(iX,iY,iZ,1));
+% % % %         PeakStore(iX,iY,iZ,2) = 1./f1(PeakStore(iX,iY,iZ,2));
+% % % %       end
+% % % %     end
+
   end
 end
+
+
                           
